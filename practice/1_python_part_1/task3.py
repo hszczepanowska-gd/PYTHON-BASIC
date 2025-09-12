c525d3e5d4558e3f0bccf032db05b64b0805f06f
Helena <hszczepanowska@griddynamics.com>
2025-09-12 13:10:33 +0200

    Add solutions for part 1 tasks

--- practice/1_python_part_1/task3.py
+++ practice/1_python_part_1/task3.py
@@ -16,4 +16,27 @@ from typing import Iterable
 
 
 def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
-    ...
+    result = []
+    for line in lines:
+        unique_set = set()
+        unique_words = []
+        words = line.split()
+        
+        for word in words:
+            if word not in unique_set:
+                unique_set.add(word)
+                unique_words.append(word)
+    
+        if len(unique_words) > word_number:
+            result.append(unique_words[word_number])
+    
+    return ' '.join(result)
+
+
+def test_build_from_unique_words():
+    assert build_from_unique_words('a b c', '1 1 1 2 3', 'cat dog milk', word_number=1) == 'b 2 dog'
+    assert build_from_unique_words('a b c', '', 'cat dog milk', word_number=0) == 'a cat'
+    assert build_from_unique_words('1 2', '1 2 3', word_number=10) == ''
+    assert build_from_unique_words(word_number=10) == ''
+
+test_build_from_unique_words()
--- practice/1_python_part_1/task5.py
+++ practice/1_python_part_1/task5.py
@@ -13,4 +13,12 @@ Examples:
 
 
 def remove_duplicated_words(line: str) -> str:
-    ...
+    unique_words = list(dict.fromkeys(line.split()))
+    return ' '.join(unique_words)
+
+def test_remove_duplicated_words():
+    assert remove_duplicated_words('cat cat dog 1 dog 2') == 'cat dog 1 2'
+    assert remove_duplicated_words('cat cat cat') == 'cat'
+    assert remove_duplicated_words('1 2 3') == '1 2 3'
+
+test_remove_duplicated_words()
